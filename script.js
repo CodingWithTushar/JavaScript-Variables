4843b9161952e492517cfbd39873b0f34a86aa50
Tushar <tushard3998@gmail.com>
2024-02-06 13:02:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+console.log("Tutorail 2 Of JavaScript")
+
+/*var a = 5;
+
+var b = 6;
+
+var c = "Hello Coders";
+
+var _d = 9;*/
+
+let a = 5;
+
+let b = 6;
+
+let c = "Hello Coders";
+
+let _d = 9;
+
+//Not Allowed
+//var 23a = 97;
+
+console.log(a+b+9)
+
+console.log(typeof a ,typeof b ,typeof c)
+{
+    let a = 98
+    console.log(a)
+}
+console.log(a)
+
+//Not Allowed a1 is constant 
+//const a1 = 9;
+//a1 = a1+0;
+
+//Primitive Data Type
+
+let x = "Coders";
+let y = 22;
+let z = 3.55;
+const h = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,h,q,r)
+console.log(typeof x, typeof y,typeof z,typeof h,typeof q,typeof r)
+
+let o = {
+    "name":"Tushar",
+    "job role":"Full stack developer",
+    "isHandsome":true,
+}
+
+console.log(o)
+o.salary = "10k"
+console.log(o)
+
+const Students_Details = {
+
+    "name":"King shit",
+    "Number":"99903444XX",
+    "Result":"false",
+
+}
+
+console.log(Students_Details)
